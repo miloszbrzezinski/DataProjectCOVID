614dbdd234483352664ed333069dd9712ee65aaa
miloszbrzezinski <109414155+miloszbrzezinski@users.noreply.github.com>
2024-04-21 16:02:32 +0200

    COVID vaccinations queries added

--- Main.sql
+++ Main.sql
@@ -93,3 +93,91 @@ SELECT
 FROM CovidDeaths
 WHERE continent IS NOT NULL
 ORDER BY 1, 2
+
+
+WITH POPvsVAC (continent, location, date, population, new_vaccinations, RollingPeopleVaccinated)
+AS
+(
+SELECT 
+    death.continent,
+    death.[location],
+    death.[date],
+    death.population,
+    vaccs.new_vaccinations,
+    SUM(vaccs.new_vaccinations) 
+    OVER 
+        (PARTITION BY 
+            death.location 
+        ORDER BY 
+            death.location, 
+            death.date)
+        AS RollingPeopleVaccinated
+FROM CovidDeaths death
+JOIN CovidVaccinations vaccs
+    ON death.[location] = vaccs.[location]
+    AND death.[date] = vaccs.[date]
+WHERE death.continent IS NOT NULL
+)
+SELECT *, (CONVERT(float, RollingPeopleVaccinated)/population)*100 FROM POPvsVAC
+
+
+-- TEMP TABLE
+
+DROP TABLE IF EXISTS #PercentPopulatioVaccinated
+CREATE TABLE #PercentPopulatioVaccinated
+(
+    continent NVARCHAR(255),
+    location NVARCHAR(255),
+    date DATETIME,
+    population NUMERIC,
+    new_vaccination NUMERIC,
+    rolling_people_vaccinated NUMERIC
+)
+
+INSERT INTO #PercentPopulatioVaccinated
+SELECT 
+    death.continent,
+    death.[location],
+    death.[date],
+    death.population,
+    vaccs.new_vaccinations,
+    SUM(vaccs.new_vaccinations) 
+    OVER 
+        (PARTITION BY 
+            death.location 
+        ORDER BY 
+            death.location, 
+            death.date)
+        AS RollingPeopleVaccinated
+FROM CovidDeaths death
+JOIN CovidVaccinations vaccs
+    ON death.[location] = vaccs.[location]
+    AND death.[date] = vaccs.[date]
+WHERE death.continent IS NOT NULL
+
+
+SELECT * FROM #PercentPopulatioVaccinated
+
+
+-- Creating view to store data for later visualistions
+
+CREATE VIEW PercentPopulatioVaccinated AS
+SELECT 
+    death.continent,
+    death.[location],
+    death.[date],
+    death.population,
+    vaccs.new_vaccinations,
+    SUM(vaccs.new_vaccinations) 
+    OVER 
+        (PARTITION BY 
+            death.location 
+        ORDER BY 
+            death.location, 
+            death.date)
+        AS RollingPeopleVaccinated
+FROM CovidDeaths death
+JOIN CovidVaccinations vaccs
+    ON death.[location] = vaccs.[location]
+    AND death.[date] = vaccs.[date]
+WHERE death.continent IS NOT NULL
